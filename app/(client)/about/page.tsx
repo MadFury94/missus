2321309f226bec56f61000058be9f72bdc6ed4ad
MadFury <onochieazukaeme@gmail.com>
2026-09-04 06:14:15 +0100

    u[date

--- app/(client)/about/page.tsx
+++ app/(client)/about/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
-function useInView(threshold) {
+function useInView(threshold?: number) {
     const ref = useRef(null);
     const [inView, setInView] = useState(false);
     useEffect(() => {
