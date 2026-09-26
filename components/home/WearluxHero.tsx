bbd3cd7743e368b0a5fc15c7dcffd37234d617c7
MadFury <onochieazukaeme@gmail.com>
2026-09-26 16:42:50 +0100

    Add Wearlux hero carousel imagery

--- components/home/WearluxHero.tsx
+++ components/home/WearluxHero.tsx
@@ -6,8 +6,9 @@ import Link from "next/link";
 
 const slides = [
     { image: "/wearlux/hero-editorial-photo.png", title: "STYLE,\nREDEFINED.", sub: "Contemporary menswear designed for the modern man." },
-    { image: "/wearlux/signature-linen-set.png", title: "EVERYDAY\nLUXURY.", sub: "Refined pieces for warm days and elevated occasions." },
     { image: "/wearlux/hero.png", title: "RESORT\nESSENTIALS.", sub: "Lightweight knitwear made for sunlit days and slow evenings." },
+    { image: "/wearlux/hero2.png", title: "LIGHT\nLAYERS.", sub: "Relaxed tailoring for every sunlit occasion." },
+    { image: "/wearlux/hero3-wearlux.png", title: "THE\nWEARLUX EDIT.", sub: "Statement pieces with an effortless point of view." },
 ];
 
 export default function WearluxHero() {
